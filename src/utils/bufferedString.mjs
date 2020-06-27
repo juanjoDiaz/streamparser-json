0b186107cac5b65b2d5befe351a6fa941d5ee1dc
Juanjo Diaz <juanjo.diaz@intopalo.com>
2020-06-27 11:52:20 +0200

    Cache BufferedString byteLength during construction

--- src/utils/bufferedString.mjs
+++ src/utils/bufferedString.mjs
@@ -1,49 +1,42 @@
 export class NonBufferedString {
   constructor() {
-    this.string = undefined;
     this.decoder = new TextDecoder('utf-8');
+    this.reset();
   }
   appendChar(char) {
     this.string += String.fromCharCode(char);
+    this.byteLength += 1;
   }
   appendBuf(buf, start = 0, end = buf.length) {
     this.string += this.decoder.decode(buf.subarray(start, end));
+    this.byteLength += end - start;
   }
   reset() {
     this.string = "";
+    this.byteLength = 0;
   }
   toString() {
     return this.string;
-  }
-  byteLength() {
-    let bytes = this.string.length;
-    for (let i = this.string.length-1; i >= 0; i--) {
-      const code = this.string.charCodeAt(i);
-      if (code <= 0x7F) continue;
-      else if (code <= 0x7FF) bytes += 1;
-      else if (code <= 0xFFFF) bytes += 2;
-      // else if (code <= 0xDFFF) i -= 1; //trail surrogate already removed by the tokenizer
-    }
-    return bytes;
   }
 }
 
 export class BufferedString {
   constructor(bufferSize) {
-    this.string = undefined;
-    this.buffer = new Uint8Array(bufferSize);
-    this.bufferOffset = 0;
     this.decoder = new TextDecoder('utf-8');
+    this.buffer = new Uint8Array(bufferSize);
+    this.reset();
   }
   appendChar(char) {
     if (this.bufferOffset >= this.buffer.length) this.flushStringBuffer();
     this.buffer[this.bufferOffset++] = char;
+    this.byteLength += 1;
   }
   appendBuf(buf, start = 0, end = buf.length) {
     const size = end - start;
     if (this.bufferOffset + size > this.buffer.length) this.flushStringBuffer();
     this.buffer.set(buf.subarray(start, end), this.bufferOffset);
     this.bufferOffset += size;
+    this.byteLength += size;
   }
   flushStringBuffer() {
     this.string += this.decoder.decode(this.buffer.subarray(0, this.bufferOffset));
@@ -52,20 +45,10 @@ export class BufferedString {
   reset() {
     this.string = "";
     this.bufferOffset = 0;
+    this.byteLength = 0;
   }
   toString() {
     this.flushStringBuffer();
     return this.string;
-  }
-  byteLength() {
-    let bytes = this.string.length;
-    for (let i = this.string.length-1; i >= 0; i--) {
-      const code = this.string.charCodeAt(i);
-      if (code <= 0x7F) continue;
-      else if (code <= 0x7FF) bytes += 1;
-      else if (code <= 0xFFFF) bytes += 2;
-      // else if (code <= 0xDFFF) i -= 1; //trail surrogate already removed by the tokenizer
-    }
-    return bytes;
   }
 }
