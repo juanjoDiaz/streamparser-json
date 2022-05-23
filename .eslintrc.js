a22e643d5c0a5b42ae7e9cbb38543c2f26f3a4c5
Juanjo Diaz <juanjo.diazmo@gmail.com>
2022-05-23 14:58:13 +0200

    chore: add windows and newer node versions to CI pipeline

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,8 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "prettier/prettier": ["error", {
+      "endOfLine": "auto"
+    }]
   },
 };
