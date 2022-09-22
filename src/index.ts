b3dbde8b0ceb3ec4bac96c434eeb6ff0508c5847
drawmindmap <drawmindmap@gmail.com>
2022-09-22 09:34:25 +0800

    chore: fix types and module path in package.json

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as TokenParser } from "./tokenparser";
 export { default as JSONParser } from "./jsonparser";
 export * as utf8 from "./utils/utf-8";
 export { TokenType } from "./utils/constants";
+export * from "./utils/types";
