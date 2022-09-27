f9c2da5908028d9807ead5943612c7f9ddaed1a5
Juanjo Diaz <juanjo.diazmo@gmail.com>
2022-09-27 20:00:23 +0200

    refactor: export types within a types object

--- dist/deno/index.ts
+++ dist/deno/index.ts
@@ -1,6 +1,6 @@
 export { default as Tokenizer } from './tokenizer.ts';
 export { default as TokenParser } from './tokenparser.ts';
 export { default as JSONParser } from './jsonparser.ts';
-export * as utf8 from './utils/utf-8.ts';
 export { TokenType } from './utils/constants.ts';
-export * from './utils/types.ts'
+export * as types from './utils/types.ts';
+export * as utf8 from './utils/utf-8.ts';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as Tokenizer } from "./tokenizer";
 export { default as TokenParser } from "./tokenparser";
 export { default as JSONParser } from "./jsonparser";
-export * as utf8 from "./utils/utf-8";
 export { TokenType } from "./utils/constants";
-export * from "./utils/types";
+export * as types from "./utils/types";
+export * as utf8 from "./utils/utf-8";
